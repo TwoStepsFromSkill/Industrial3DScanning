303ade28dc114a6bbe960665b23925a6db00ff61
Daniel Sopauschke <daniel.sopauschke@st.ovgu.de>
2017-12-06 15:54:47 +0100

    Added documentation for the widget classes

--- include/BestFitPlaneWidget.h
+++ include/BestFitPlaneWidget.h
@@ -7,19 +7,44 @@ class QVBoxLayout;
 class QPushButton;
 class QGroupBox;
 
+/**
+ * @brief Widget that provides a button to start the best plane/line fit
+ * algorithm.
+ */
 class BestFitPlaneWidget : public BaseTabWidget
 {
     Q_OBJECT
 public:
+    /**
+     * @brief Constructor.
+     * @param parent Pointer to the parent element. (nullptr by default)
+     */
     BestFitPlaneWidget(QWidget* parent = nullptr);
+
+    /**
+     * @brief Destructor.
+     * @note Needs to be virtual since we are in a base class of an abstract
+     *          class.
+     */
     virtual ~BestFitPlaneWidget() {}
 
+    /**
+     * @brief Deactivate the widget components.
+     */
     virtual void deactivate() override;
+    /**
+     * @brief Activate the widget components.
+     */
     virtual void activate() override;
 
 signals:
+    /**
+     * @brief Signal to inform listeners that the widget was enable or disabled.
+     */
     void widgetEnabled(bool value);
-
+    /**
+     * @brief Signal to inform listeners that the apply button was pressed.
+     */
     void applyPressed();
 
 private:
--- include/RangeQueryWidget.h
+++ include/RangeQueryWidget.h
@@ -10,31 +10,83 @@ class QSlider;
 class QGroupBox;
 class QCheckBox;
 
+/**
+ * @brief Widget that provides options to set up the range query.
+ */
 class RangeQueryWidget : public BaseTabWidget
 {
     Q_OBJECT
 public:
+    /**
+     * @brief Constructor.
+     * @param parent Pointer to the parent element. (nullptr by default)
+     */
     RangeQueryWidget(QWidget* parent = nullptr);
+    /**
+     * @brief Destructor.
+     * @note Needs to be virtual since we are in a base class of an abstract
+     *          class.
+     */
     virtual ~RangeQueryWidget() {}
 
+    /**
+     * @brief Reset the range of possible coordiante values in x,y,z axis.
+     *
+     * This method has to called if the dataset changed. The widget needs
+     * this information to adapt the range of the sliders to fit the dataset
+     * extents.
+     */
     void resetValueRange(double xMin, double xMax,
                        double yMin, double yMax,
                        double zMin, double zMax);
 
+    /**
+     * @brief Get the extents of the range query axis aligned box.
+     * @param[in,out] minMax 6-element double array that is filled with the
+     *                          extents of the range query box in the order
+     *                          minX, maxX, minY, maxY, minZ, maxZ
+     */
     void getBox(double* minMax);
 
+    /**
+     * @brief Deactivate the widget components.
+     */
     virtual void deactivate() override;
+    /**
+     * @brief Activate the widget components.
+     */
     virtual void activate() override;
 
 signals:
+    /**
+     * @brief Signal to inform listeners that the widget was enable or disabled.
+     */
     void widgetEnabled(bool value);
 
+    /**
+     * @brief Signal to inform listeners that the center of the box changed
+     *          to x, y, z.
+     */
     void centerChanged(double x, double y, double z);
+    /**
+     * @brief Signal to inform listeners that the box extents changed
+     *          to dx, dy, dz.
+     */
     void extendChanged(double dx, double dy, double dz);
 
+    /**
+     * @brief Signal to inform listeners that the live update option was toggled
+     *          and has a new value.
+     */
     void liveUpdateChanged(bool value);
 
+    /**
+     * @brief Signal to inform listeners that the apply button was pressed.
+     */
     void applyPressed();
+    /**
+     * @brief Signal to inform listeners that the hide button was pressed.
+     */
     void hidePressed();
 
 private:
